84be459def34da314912fbbe63d0062cf067714c
johnnyrepo <ds@dsf.sd>
2013-05-31 08:25:12 +0300

    zoom steps

--- kinect_psy_manager/src/ee/ttu/kinect/view/TracingPanel.java
+++ kinect_psy_manager/src/ee/ttu/kinect/view/TracingPanel.java
@@ -23,117 +23,117 @@ public abstract class TracingPanel extends JPanel {
 	
 	protected Graphics2D graphics;
 
-	private JLabel coordLabel;
+	private final JLabel coordLabel;
 
 	private Body body;
 
 	private boolean seatedMode;
 	
-	private int[] zoom = {100, 150, 200};
+	private final int[] zoom = {100, 125, 150, 175, 200};
 	
-	private int zoomStep = zoom.length - 1;
+	private int zoomStep = this.zoom.length - 1;
 
 	protected TracingPanel(String title) {
 		Border border = BorderFactory.createEtchedBorder();
-		setBorder(BorderFactory.createTitledBorder(border, title));
-		coordLabel = new JLabel();
-		add(coordLabel, BorderLayout.NORTH);
+		this.setBorder(BorderFactory.createTitledBorder(border, title));
+		this.coordLabel = new JLabel();
+		this.add(this.coordLabel, BorderLayout.NORTH);
 	}
 
 	public void redrawSkeleton(Body body, boolean seatedMode) {
 		this.body = body;
 		this.seatedMode = seatedMode;
 		
-		setSpinePosition(body);
+		this.setSpinePosition(body);
 		
-		repaint();
+		this.repaint();
 	}
 	
 	@Override
 	public synchronized void paint(Graphics g) {
 		super.paint(g);
-		if (body != null && body.isBodyReady()) {
+		if (this.body != null && this.body.isBodyReady()) {
 			// draw joint connection
 			this.graphics = (Graphics2D) g;
-			drawJointLine(body.getHead(), body.getShoulderCenter());
-			drawJointLine(body.getShoulderCenter(), body.getShoulderLeft());
-			drawJointLine(body.getShoulderLeft(), body.getElbowLeft());
-			drawJointLine(body.getElbowLeft(), body.getWristLeft());
-			drawJointLine(body.getWristLeft(), body.getHandLeft());
-			drawJointLine(body.getShoulderCenter(), body.getShoulderRight());
-			drawJointLine(body.getShoulderRight(), body.getElbowRight());
-			drawJointLine(body.getElbowRight(), body.getWristRight());
-			drawJointLine(body.getWristRight(), body.getHandRight());
+			this.drawJointLine(this.body.getHead(), this.body.getShoulderCenter());
+			this.drawJointLine(this.body.getShoulderCenter(), this.body.getShoulderLeft());
+			this.drawJointLine(this.body.getShoulderLeft(), this.body.getElbowLeft());
+			this.drawJointLine(this.body.getElbowLeft(), this.body.getWristLeft());
+			this.drawJointLine(this.body.getWristLeft(), this.body.getHandLeft());
+			this.drawJointLine(this.body.getShoulderCenter(), this.body.getShoulderRight());
+			this.drawJointLine(this.body.getShoulderRight(), this.body.getElbowRight());
+			this.drawJointLine(this.body.getElbowRight(), this.body.getWristRight());
+			this.drawJointLine(this.body.getWristRight(), this.body.getHandRight());
 			
 			// draw joint
-			drawJoint(body.getShoulderCenter());
-			drawJoint(body.getShoulderLeft());
-			drawJoint(body.getShoulderLeft());
-			drawJoint(body.getShoulderRight());
-			drawJoint(body.getElbowLeft());
-			drawJoint(body.getElbowRight());
-			drawJoint(body.getWristLeft());
-			drawJoint(body.getWristRight());
-			drawJoint(body.getHandLeft());
-			drawJoint(body.getHandRight());
+			this.drawJoint(this.body.getShoulderCenter());
+			this.drawJoint(this.body.getShoulderLeft());
+			this.drawJoint(this.body.getShoulderLeft());
+			this.drawJoint(this.body.getShoulderRight());
+			this.drawJoint(this.body.getElbowLeft());
+			this.drawJoint(this.body.getElbowRight());
+			this.drawJoint(this.body.getWristLeft());
+			this.drawJoint(this.body.getWristRight());
+			this.drawJoint(this.body.getHandLeft());
+			this.drawJoint(this.body.getHandRight());
 			
-			if (!seatedMode) {
-				drawJointLine(body.getShoulderCenter(), body.getSpine());
-				drawJointLine(body.getSpine(), body.getHipCenter());
-				drawJointLine(body.getHipCenter(), body.getHipLeft());
-				drawJointLine(body.getHipLeft(), body.getKneeLeft());
-				drawJointLine(body.getKneeLeft(), body.getAnkleLeft());
-				drawJointLine(body.getAnkleLeft(), body.getFootLeft());
-				drawJointLine(body.getHipCenter(), body.getHipRight());
-				drawJointLine(body.getHipRight(), body.getKneeRight());
-				drawJointLine(body.getKneeRight(), body.getAnkleRight());
-				drawJointLine(body.getAnkleRight(), body.getFootRight());
+			if (!this.seatedMode) {
+				this.drawJointLine(this.body.getShoulderCenter(), this.body.getSpine());
+				this.drawJointLine(this.body.getSpine(), this.body.getHipCenter());
+				this.drawJointLine(this.body.getHipCenter(), this.body.getHipLeft());
+				this.drawJointLine(this.body.getHipLeft(), this.body.getKneeLeft());
+				this.drawJointLine(this.body.getKneeLeft(), this.body.getAnkleLeft());
+				this.drawJointLine(this.body.getAnkleLeft(), this.body.getFootLeft());
+				this.drawJointLine(this.body.getHipCenter(), this.body.getHipRight());
+				this.drawJointLine(this.body.getHipRight(), this.body.getKneeRight());
+				this.drawJointLine(this.body.getKneeRight(), this.body.getAnkleRight());
+				this.drawJointLine(this.body.getAnkleRight(), this.body.getFootRight());
 				
-				drawJoint(body.getSpine());
-				drawJoint(body.getHipCenter());
-				drawJoint(body.getHipLeft());
-				drawJoint(body.getHipRight());
-				drawJoint(body.getKneeLeft());
-				drawJoint(body.getKneeRight());
-				drawJoint(body.getAnkleLeft());
-				drawJoint(body.getAnkleRight());
-				drawJoint(body.getFootLeft());
-				drawJoint(body.getFootRight());
+				this.drawJoint(this.body.getSpine());
+				this.drawJoint(this.body.getHipCenter());
+				this.drawJoint(this.body.getHipLeft());
+				this.drawJoint(this.body.getHipRight());
+				this.drawJoint(this.body.getKneeLeft());
+				this.drawJoint(this.body.getKneeRight());
+				this.drawJoint(this.body.getAnkleLeft());
+				this.drawJoint(this.body.getAnkleRight());
+				this.drawJoint(this.body.getFootLeft());
+				this.drawJoint(this.body.getFootRight());
 			}
 
-			drawHead(body.getHead());
-			updateCoordLabel(body.getHead());
+			this.drawHead(this.body.getHead());
+			this.updateCoordLabel(this.body.getHead());
 		}
 	}
 
 	public void clear() {
 		this.body = null;
-		repaint();
+		this.repaint();
 	}
 	
 	protected void drawJointLine(Joint joint1, Joint joint2) {
-		if (joint1 != null && joint2 != null && graphics != null) {
-			int x1 = getXForGraph(joint1);
-			int y1 = getYForGraph(joint1);
-			int x2 = getXForGraph(joint2);
-			int y2 = getYForGraph(joint2);
-			graphics.drawLine(x1, y1, x2, y2);
+		if (joint1 != null && joint2 != null && this.graphics != null) {
+			int x1 = this.getXForGraph(joint1);
+			int y1 = this.getYForGraph(joint1);
+			int x2 = this.getXForGraph(joint2);
+			int y2 = this.getYForGraph(joint2);
+			this.graphics.drawLine(x1, y1, x2, y2);
 		}
 	}
 
 	protected void drawHead(Joint head)  {
-		int x = getXForGraph(head);
-		int y = getYForGraph(head);
-		graphics.drawOval(x - 10, y - 10, 20, 20);
+		int x = this.getXForGraph(head);
+		int y = this.getYForGraph(head);
+		this.graphics.drawOval(x - 10, y - 10, 20, 20);
 	}
 
 	protected void drawJoint(Joint joint) {
 		if (joint != null) {
-			int x = getXForGraph(joint);
-			int y = getYForGraph(joint);
-			graphics.setColor(new Color(0, 160, 0));
-			graphics.fillOval(x - 3, y - 3, 6, 6);
-			graphics.setColor(Color.BLACK);
+			int x = this.getXForGraph(joint);
+			int y = this.getYForGraph(joint);
+			this.graphics.setColor(new Color(0, 160, 0));
+			this.graphics.fillOval(x - 3, y - 3, 6, 6);
+			this.graphics.setColor(Color.BLACK);
 		}
 	}
 
@@ -144,19 +144,19 @@ public abstract class TracingPanel extends JPanel {
 	abstract protected int getYForGraph(Joint joint);
 	
 	protected void zoomIn() {
-		if (++zoomStep >= zoom.length) {
-			zoomStep = zoom.length - 1;
+		if (++this.zoomStep >= this.zoom.length) {
+			this.zoomStep = this.zoom.length - 1;
 		}
 	}
 	
 	protected void zoomOut() {
-		if (--zoomStep < 0) {
-			zoomStep = 0;
+		if (--this.zoomStep < 0) {
+			this.zoomStep = 0;
 		}
 	}
 	
 	protected int getZoomValue() {
-		return zoom[zoomStep];
+		return this.zoom[this.zoomStep];
 	}
 	
 	private void updateCoordLabel(Joint joint) {
@@ -164,7 +164,7 @@ public abstract class TracingPanel extends JPanel {
 			double x = (double) Math.round(joint.getPositionX() * 100) / 100;
 			double y = (double) Math.round(joint.getPositionY() * 100) / 100;
 			double z = (double) Math.round(joint.getPositionZ() * 100) / 100;
-			coordLabel.setText("X = " + x + ", Y = " + y + ", Z = " + z);
+			this.coordLabel.setText("X = " + x + ", Y = " + y + ", Z = " + z);
 		}
 	}
 
