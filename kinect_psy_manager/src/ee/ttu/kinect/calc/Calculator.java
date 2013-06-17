93fbad1fe7f7466cd5bdb9fd3bd2ff331b929169
johnnyrepo <ds@dsf.sd>
2013-06-17 21:05:23 +0300

    Calculation for Trajectory mass fixed a bit

--- kinect_psy_manager/src/ee/ttu/kinect/calc/Calculator.java
+++ kinect_psy_manager/src/ee/ttu/kinect/calc/Calculator.java
@@ -1,22 +1,25 @@
 package ee.ttu.kinect.calc;
 
+import java.util.Locale;
+
 import ee.ttu.kinect.model.Joint;
 
 public class Calculator {
 
-	// Calculates velocity in 3D space
+	// Calculates velocity in 3D space (m/s)
 	// v = sqrt((x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2) / t
 	public static double calculateVelocity3D(Joint joint1, Joint joint2, long time1, long time2) {
 		double s1 = joint1.getPositionX() - joint2.getPositionX();
 		double s2 = joint1.getPositionY() - joint2.getPositionY();
 		double s3 = joint1.getPositionZ() - joint2.getPositionZ();
-		return Math.sqrt(s1*s1 + s2*s2 + s3*s3) / (time1 - time2);
+		return (Math.sqrt(s1*s1 + s2*s2 + s3*s3) / (time1 - time2) * 1000);
 	}
 	
-	// Calculates acceleration in 3D space
+	// Calculates acceleration in 3D space (m/s^2)
 	// a = v / t
 	public static double calculateAcceleration3D(Joint joint1, Joint joint2, long time1, long time2) {
-		return Calculator.calculateVelocity3D(joint1, joint2, time1, time2) / (time1 - time2);
+		System.out.print(" " + String.format(Locale.ENGLISH, "%-14.13f", Calculator.calculateVelocity3D(joint1, joint2, time1, time2)));
+		return (Calculator.calculateVelocity3D(joint1, joint2, time1, time2) / (time1 - time2) * 1000);
 	}
 	
 	// Calculates trajectory in 3D space
--- kinect_psy_manager/src/ee/ttu/kinect/view/chart/MovementDetectionAnalyzer.java
+++ kinect_psy_manager/src/ee/ttu/kinect/view/chart/MovementDetectionAnalyzer.java
@@ -1,9 +1,10 @@
 package ee.ttu.kinect.view.chart;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -12,6 +13,7 @@ import javax.swing.JPanel;
 
 import ee.ttu.kinect.calc.Calculator;
 import ee.ttu.kinect.model.Body;
+import ee.ttu.kinect.model.Joint;
 import ee.ttu.kinect.model.JointType;
 
 
@@ -33,23 +35,25 @@ public class MovementDetectionAnalyzer {
 		private JPanel summaryPanel;
 		
 		private MovementDetectionChartFrame(List<Body> data, JointType type, 
-				double trajectoryMassSummary, double accelerationMassSummary) {		
-			//setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+				double trajectoryMassSummary, double accelerationMassSummary) {
+			setTitle("Movement has been detected!");
 			
-			setSize(600, 500);
+			setSize(1200, 400);
+
+			getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
 			
 			chart = new MovementChart();
-			chart.setPreferredSize(new Dimension(600, 260));
+			chart.setPreferredSize(new Dimension(800, 400));
 			chartPanel = new JPanel();
 			chartPanel.add(chart);
 			
 			summaryPanel = new JPanel();
 			
-			add(chartPanel, BorderLayout.CENTER);
-			add(summaryPanel, BorderLayout.SOUTH);
+			getContentPane().add(summaryPanel);//, BorderLayout.SOUTH);
+			getContentPane().add(chartPanel);//, BorderLayout.CENTER);
 			
 			drawChart(data, type);
-			outputSummary(data, type, trajectoryMassSummary, accelerationMassSummary);
+			outputSummary(data, type);
 			
 			setVisible(true);
 		}
@@ -60,31 +64,80 @@ public class MovementDetectionAnalyzer {
 			chart.drawChart(data, types, false);
 		}
 		
-		private void outputSummary(List<Body> data, JointType type, double trajectoryMassSummary, double accelerationMassSummary) {
+		private void outputSummary(List<Body> data, JointType type) {
 			long frameStart = data.get(0).getFrameNumber();
 			long frameEnd = data.get(data.size() - 1).getFrameNumber();
-			double startTime = data.get(0).getTimestamp() / 1000;
-			double endTime = data.get(data.size() - 1).getTimestamp() / 1000;
-			double time = endTime - startTime;
-			double eucledianDistance = Calculator.calculateTrajectoryLength3D(data.get(0).getJoint(type), 
-					data.get(data.size() - 1).getJoint(type));
+			double startTime = data.get(0).getTimestamp();
+			double endTime = data.get(data.size() - 1).getTimestamp();
+			double time = (endTime - startTime) / 1000;
+			double trajectoryMassSummary = calculateTrajectoryMass(data, type);
+			double accelerationMassSummary = calculateAccelerationMass(data, type);
+			double eucledianDistance = calculateEucledianDistance(data, type);
 			double ratio = eucledianDistance / trajectoryMassSummary;
 			
 			summaryPanel.setLayout(new BoxLayout(summaryPanel, BoxLayout.Y_AXIS));
-			summaryPanel.add(new JLabel("Frame start: " + frameStart));
-			summaryPanel.add(new JLabel("Frame end: " + frameEnd));
-			summaryPanel.add(new JLabel("Time elapsed: " + time));
-			summaryPanel.add(new JLabel("Trajectory mass: " + trajectoryMassSummary));
-			summaryPanel.add(new JLabel("Acceleration mass: " + accelerationMassSummary));
-			summaryPanel.add(new JLabel("(Eucledian distance / Trajecory mass) ratio: " + ratio));
+			summaryPanel.add(createLabel("Frame start: " + frameStart));
+			summaryPanel.add(createLabel("Frame end: " + frameEnd));
+			summaryPanel.add(createLabel("Time elapsed: " + time));
+			summaryPanel.add(createLabel("Eucledian distance: " + String.format(Locale.ENGLISH, "%-6.3f", eucledianDistance)));
+			summaryPanel.add(createLabel("Trajectory mass: " + String.format(Locale.ENGLISH, "%-6.3f", trajectoryMassSummary)));
+			summaryPanel.add(createLabel("Acceleration mass: " + String.format(Locale.ENGLISH, "%-6.3f", accelerationMassSummary)));
+			summaryPanel.add(createLabel("Ratio (Eucl. dist. / Traj. mass): " + String.format(Locale.ENGLISH, "%-6.3f", ratio)));
 			
 			System.out.println("Frame start: " + frameStart);
 			System.out.println("Frame end: " + frameEnd);
 			System.out.println("Time elapsed: " + time);
+			System.out.println("Eucledian distance: " + eucledianDistance);
 			System.out.println("Trajectory mass: " + trajectoryMassSummary);
 			System.out.println("Acceleration mass: " + accelerationMassSummary);
-			System.out.println("Eucledian distance: " + eucledianDistance);
 			System.out.println("Eucl / Traj ratio: " + ratio);
+		}
+
+		private JLabel createLabel(String text) {
+			JLabel label = new JLabel(text);
+			label.setFont(new Font("Serif", Font.BOLD, 16));
+			
+			return label;
+		}
+		
+		private double calculateTrajectoryMass(List<Body> data, JointType type) {
+			double trajectoryMass = 0;
+			for (int i = 0; i < data.size(); i++) {
+				if (i + 1 >= data.size()) {
+					break;
+				}
+				Joint joint1 = data.get(i).getJoint(type);
+				Joint joint2 = data.get(i + 1).getJoint(type);
+				trajectoryMass += Calculator.calculateTrajectoryLength3D(joint1, joint2);
+			}
+			
+			return trajectoryMass;
+		}
+		
+		private double calculateAccelerationMass(List<Body> data, JointType type) {
+			double accelerationMass = 0;
+			for (int i = 0; i < data.size(); i++) {
+				if (i + 1 >= data.size()) {
+					break;
+				}
+				Joint joint1 = data.get(i).getJoint(type);
+				Joint joint2 = data.get(i + 1).getJoint(type);
+				long time1 = data.get(i).getTimestamp();
+				long time2 = data.get(i + 1).getTimestamp();
+				System.out.print(data.get(i).getFrameNumber());
+				accelerationMass += Math.abs(Calculator.calculateAcceleration3D(joint1, joint2, time1, time2));
+				System.out.println();
+				System.out.println("accel " + data.get(i).getFrameNumber() + " " + String.format(Locale.ENGLISH, "%-14.13f", Math.abs(Calculator.calculateAcceleration3D(joint1, joint2, time1, time2))));
+			}
+			System.out.println("\n\n\n");
+			
+			return accelerationMass;
+		}
+		
+		private double calculateEucledianDistance(List<Body> data,
+				JointType type) {
+			return Calculator.calculateTrajectoryLength3D(data.get(0).getJoint(type), 
+					data.get(data.size() - 1).getJoint(type));
 		}
 		
 	}
