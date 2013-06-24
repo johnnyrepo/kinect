073ba811b40068ee2c33ffe1f67b6d8d385e7593
johnnyrepo <ds@dsf.sd>
2013-06-24 21:56:15 +0300

    style

--- kinect_psy_manager/src/ee/ttu/kinect/model/MotionProcessor.java
+++ kinect_psy_manager/src/ee/ttu/kinect/model/MotionProcessor.java
@@ -8,59 +8,59 @@ import ee.ttu.kinect.calc.Calculator;
 public class MotionProcessor {
 
 	private final static double TRAJECTORY_MASS_MIN_VALUE = 0.15;
-	
-	private final int windowSize = 30; 
-	
+
+	private final int windowSize = 30;
+
 	private double trajectoryMass;
-	
+
 	private double accelerationMass;
 
 	private double velocityMass;
-	
+
 	private List<Body> data;
-	
+
 	private List<Body> dataSummary;
-	
+
 	private double trajectoryMassSummary;
-	
+
 	private double accelerationMassSummary;
-	
+
 	private double velocityMassSummary;
-	
+
 	private long firstTimestamp;
 
 	private long delay;
-	
+
 	private JointType type;
-		
+
 	public MotionProcessor() {
 		reset();
 	}
-	
+
 	public void setDelay(long delay) {
 		this.delay = delay;
 	}
-	
+
 	public void setType(JointType type) {
 		this.type = type;
 	}
-	
+
 	public void reset() {
 		trajectoryMassSummary = 0;
 		accelerationMassSummary = 0;
 		velocityMassSummary = 0;
-		
+
 		trajectoryMass = 0;
 		accelerationMass = 0;
 		velocityMass = 0;
-		
+
 		dataSummary = new ArrayList<Body>();
 		data = new ArrayList<Body>();
-		
+
 		firstTimestamp = 0;
-		
+
 	}
-	
+
 	public boolean process(Body body) {
 		// set very first timestamp if processing has been just started
 		if (data.size() == 0) {
@@ -76,10 +76,10 @@ public class MotionProcessor {
 			analyze(data, type);
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	private void analyze(List<Body> data, JointType type) {
 		trajectoryMass = 0;
 		velocityMass = 0;
@@ -92,29 +92,33 @@ public class MotionProcessor {
 				long time1 = data.get(i).getTimestamp();
 				long time2 = data.get(i + 1).getTimestamp();
 				long time3 = data.get(i + 2).getTimestamp();
-				trajectoryMass += Calculator.calculateTrajectoryLength3D(joint2, joint3);
-				velocityMass += Math.abs(Calculator.calculateVelocity3D(joint2, joint3, time2, time3));
-				accelerationMass += Math.abs(Calculator.calculateAcceleration3D(joint1, joint2, joint3, time1, time2, time3));
+				trajectoryMass += Calculator.calculateTrajectoryLength3D(
+						joint2, joint3);
+				velocityMass += Math.abs(Calculator.calculateVelocity3D(joint2,
+						joint3, time2, time3));
+				accelerationMass += Math.abs(Calculator
+						.calculateAcceleration3D(joint1, joint2, joint3, time1,
+								time2, time3));
 			}
 		}
 	}
-	
+
 	private void clean() {
 		trajectoryMassSummary += trajectoryMass;
 		accelerationMassSummary += accelerationMass;
 		velocityMassSummary += velocityMass;
-		
+
 		trajectoryMass = 0;
 		velocityMass = 0;
 		accelerationMass = 0;
-		
+
 		data.remove(0);
 	}
-	
+
 	public double getTrajectoryMass() {
 		return trajectoryMass;
 	}
-	
+
 	public double getAccelerationMass() {
 		return accelerationMass;
 	}
@@ -122,24 +126,24 @@ public class MotionProcessor {
 	public List<Body> getData() {
 		return data;
 	}
-	
+
 	public List<Body> getDataSummary() {
 		return dataSummary;
 	}
-	
+
 	public double getTrajectoryMassSummary() {
 		return trajectoryMassSummary;
 	}
-	
+
 	public double getAccelerationMassSummary() {
 		return accelerationMassSummary;
 	}
-	
+
 	public boolean isMotionEnded() {
 		if ((data.get(data.size() - 1).getTimestamp() - firstTimestamp) >= delay) {
 			return trajectoryMass < MotionProcessor.TRAJECTORY_MASS_MIN_VALUE;
 		}
-		
+
 		return false;
 	}
 
