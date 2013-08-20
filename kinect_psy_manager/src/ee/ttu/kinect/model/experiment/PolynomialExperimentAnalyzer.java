b9d6255be8908f8a86a2e86748db1a484a058000
johnnyrepo <eugeni777@yandex.ru>
2013-08-20 10:59:35 +0300

    Experiment analyzer for diferent set of experiments

--- kinect_psy_manager/src/ee/ttu/kinect/model/experiment/PolynomialExperimentAnalyzer.java
+++ kinect_psy_manager/src/ee/ttu/kinect/model/experiment/PolynomialExperimentAnalyzer.java
@@ -33,37 +33,82 @@ public class PolynomialExperimentAnalyzer {
 	
 	private static Map<String, List<Boolean>> experimentsCorrectness = new LinkedHashMap<String, List<Boolean>>();
 	
+	// 
+	private static Map<String, Experiment> experimentsV = new LinkedHashMap<String, Experiment>();
+	
+	private static Map<String, List<Boolean>> experimentsCorrectnessV = new LinkedHashMap<String, List<Boolean>>();
+	
+	private static Map<String, Experiment> experimentsS = new LinkedHashMap<String, Experiment>();
+	
+	private static Map<String, List<Boolean>> experimentsCorrectnessS = new LinkedHashMap<String, List<Boolean>>();
+	
 	static {
-		// init experiments 
-		initExperiment("2013.07.02_17-21-20.csv", new Boolean[] {false, false, false, true, false, false, false, false, false, false});
-		initExperiment("2013.07.02_17-24-13.csv", new Boolean[] {false, false, false, false, false, false, true, true, true, false});
-		initExperiment("2013.07.02_17-27-28.csv", new Boolean[] {false, false, false, false, false, true, false, true, true, false});
-		initExperiment("2013.07.02_17-30-17.csv", new Boolean[] {false, false, false, true, false, false, false, false, false, false, false}); // 11
-		initExperiment("2013.07.02_17-33-53.csv", new Boolean[] {true, true, false, true, false, false, true, true, false, false});
-		initExperiment("2013.07.02_17-36-11.csv", new Boolean[] {false, false, false, false, true, true, false, false, true, true});
-		initExperiment("2013.07.02_17-38-47.csv", new Boolean[] {false, false, true, false, false, true, false, true, false, true, false, false}); // 12
-		initExperiment("2013.07.02_17-42-08.csv", new Boolean[] {false, false, false, false, false, false, false, false, false, false});
-		initExperiment("2013.07.02_17-44-11.csv", new Boolean[] {false, true, true, false, true, false, true, false, true, true});
-		initExperiment("2013.07.02_17-46-54.csv", new Boolean[] {false, true, false, false, false, true, true, true, false, true});
-		initExperiment("2013.07.02_17-49-00.csv", new Boolean[] {false, false, false, false, false, false, false, false, false, false});
-		initExperiment("2013.07.02_17-51-12.csv", new Boolean[] {false, true, false, true, false, false, true, false, true, true});
-		initExperiment("2013.07.02_17-52-42.csv", new Boolean[] {true, true, true, true, false, false, false, true, true, true});
-		initExperiment("2013.07.02_17-54-52.csv", new Boolean[] {false, true, false, false, true, false, false, false, true, true});
-		initExperiment("2013.07.02_17-56-52.csv", new Boolean[] {false, false, true, false, true, false, false, false, true, true});
+		// init experiments for V
+		initExperimentV("2013.07.02_17-21-20.csv", new Boolean[] {false, false, false, true, false, false, false, false, false, false});
+		initExperimentV("2013.07.02_17-24-13.csv", new Boolean[] {false, false, false, false, false, false, true, true, true, false});
+		initExperimentV("2013.07.02_17-27-28.csv", new Boolean[] {false, false, false, false, false, true, false, true, true, false});
+		initExperimentV("2013.07.02_17-30-17.csv", new Boolean[] {false, false, false, true, false, false, false, false, false, false, false}); // 11
+		initExperimentV("2013.07.02_17-33-53.csv", new Boolean[] {true, true, false, true, false, false, true, true, false, false});
+		initExperimentV("2013.07.02_17-36-11.csv", new Boolean[] {false, false, false, false, true, true, false, false, true, true});
+		initExperimentV("2013.07.02_17-38-47.csv", new Boolean[] {false, false, true, false, false, true, false, true, false, true, false, false}); // 12
+		initExperimentV("2013.07.02_17-42-08.csv", new Boolean[] {false, false, false, false, false, false, false, false, false, false});
+		initExperimentV("2013.07.02_17-44-11.csv", new Boolean[] {false, true, true, false, true, false, true, false, true, true});
+		initExperimentV("2013.07.02_17-46-54.csv", new Boolean[] {false, true, false, false, false, true, true, true, false, true});
+		initExperimentV("2013.07.02_17-49-00.csv", new Boolean[] {false, false, false, false, false, false, false, false, false, false});
+		initExperimentV("2013.07.02_17-51-12.csv", new Boolean[] {false, true, false, true, false, false, true, false, true, true});
+		initExperimentV("2013.07.02_17-52-42.csv", new Boolean[] {true, true, true, true, false, false, false, true, true, true});
+		initExperimentV("2013.07.02_17-54-52.csv", new Boolean[] {false, true, false, false, true, false, false, false, true, true});
+		initExperimentV("2013.07.02_17-56-52.csv", new Boolean[] {false, false, true, false, true, false, false, false, true, true});
 	}
 	
-	private static void initExperiment(String experimentId, Boolean[] correctness) {
-		experiments.put(experimentId, new Experiment(experimentId));
-		experimentsCorrectness.put(experimentId, Arrays.asList(correctness));
+	static {
+		// init experiments for S
+		initExperimentS("2013.07.10_17-58-22.csv" , new Boolean[] {false, false, false, false, false, false, false, true, false, false});
+		initExperimentS("2013.07.10_18-01-08.csv" , new Boolean[] {true, false, false, true, true, false, false, false, true, false});
+		initExperimentS("2013.07.10_18-03-14.csv" , new Boolean[] {false, false, false, true, true, false, true, false, true, false});
+		initExperimentS("2013.07.10_18-04-59.csv" , new Boolean[] {false, false, false, false, false, false, false, false, false, true});
+		initExperimentS("2013.07.10_18-06-50.csv" , new Boolean[] {false, true, false, false, false, true, true, false, true, true});
+		initExperimentS("2013.07.10_18-08-27.csv" , new Boolean[] {false, true, true, false, true, false, false, false, true, true});
+		initExperimentS("2013.07.10_18-10-43.csv" , new Boolean[] {false, false, false, false, true, false, true, false, true, false});
+		initExperimentS("2013.07.10_18-12-28.csv" , new Boolean[] {false, true, true, false, true, false, true, true, true, false});
+		initExperimentS("2013.07.10_18-14-21.csv" , new Boolean[] {false, true, true, false, true, false, true, true, true, false});
+		initExperimentS("2013.07.10_18-17-32.csv" , new Boolean[] {false, false, false, true, false, false, true, false, false, false});
+		initExperimentS("2013.07.10_18-21-05.csv" , new Boolean[] {false, false, false, false, false, true, false, false, true, false});
+		initExperimentS("2013.07.10_18-23-02.csv" , new Boolean[] {true, true, false, false, true, true, false, false, false, true});
+		initExperimentS("2013.07.10_18-24-39.csv" , new Boolean[] {false, false, true, true, true, false, false, false, true, false});
+		initExperimentS("2013.07.10_18-25-55.csv" , new Boolean[] {true, true, false, false, false, false, false, false, true, false});
+		initExperimentS("2013.07.10_18-27-40.csv" , new Boolean[] {false, true, true, false, false, false, false, false, false, false});
+		initExperimentS("2013.07.10_18-30-17.csv" , new Boolean[] {true, true, true, true, true, true, true, true, true, true});
+		initExperimentS("2013.07.10_18-31-57.csv" , new Boolean[] {false, true, true, true, true, false, false, true, false, true});
+		initExperimentS("2013.07.10_18-33-32.csv" , new Boolean[] {false, false, false, false, true, false, true, false, true, true});
+		initExperimentS("2013.07.10_18-35-18.csv" , new Boolean[] {false, true, false, false, false, false, false, false, true, false});
+		initExperimentS("2013.07.10_18-36-51.csv" , new Boolean[] {true, true, true, true, true, true, true, true, true, false});
+
+	}
+	
+	static {
+		// init analyzer
+		experiments.putAll(experimentsV);
+		experimentsCorrectness.putAll(experimentsCorrectnessV);
+	}
+	
+	private static void initExperimentV(String experimentId, Boolean[] correctness) {
+		experimentsV.put(experimentId, new Experiment(experimentId));
+		experimentsCorrectnessV.put(experimentId, Arrays.asList(correctness));
+	}
+	
+	private static void initExperimentS(String experimentId, Boolean[] correctness) {
+		experimentsS.put(experimentId, new Experiment(experimentId));
+		experimentsCorrectnessS.put(experimentId, Arrays.asList(correctness));
 	}
 	
 	public static void analyze(File[] files) {
 		for (File file : files) {
-			System.out.println("file name: " + file.getName());
+			System.out.println("Analyzing file: " + file.getName());
 			try {
 				fileUtil.readFile(file);
 				List<String> data = fileUtil.readAllLines();
-				Experiment experiment = prepareExperiment(file.getName(), data);
+				prepareExperiment(file.getName(), data);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
@@ -133,7 +178,7 @@ public class PolynomialExperimentAnalyzer {
 			Experiment exp = expIterator.next();
 			double forecast = model.forecast(dp);
 			double real = getExperimentRealValue(exp, measure);
-			System.out.println("Forecast with indepent variable " + dp.getIndependentValue("Ind") + ": " 
+			System.out.println("Forecast with independent variable " + dp.getIndependentValue("Ind") + ": " 
 					+ forecast + ", but Real: " + real + " and Diff(Forecast-Real): " + (forecast-real));
 		}
 		System.out.println("==============================\n");
