651a50ae2f2964499bf9b95afc5636e4701f545f
johnnyrepo <eugeni777@yandex.ru>
2013-09-22 22:54:28 +0300

    calculation

--- kinect_psy_manager/src/ee/ttu/kinect/calc/Calculator.java
+++ kinect_psy_manager/src/ee/ttu/kinect/calc/Calculator.java
@@ -19,7 +19,7 @@ public class Calculator {
 		double s1 = joint2.getPositionX() - joint1.getPositionX();
 		double s2 = joint2.getPositionY() - joint1.getPositionY();
 		double s3 = joint2.getPositionZ() - joint1.getPositionZ();
-		return (Math.sqrt(s1*s1 + s2*s2 + s3*s3) / (time2 - time1) * 1000);
+		return (Math.sqrt(s1*s1 + s2*s2 + s3*s3) / (time2 - time1));
 	}
 	
 	// Calculates acceleration in 3D space (m/s^2)
@@ -28,14 +28,14 @@ public class Calculator {
 		double v1 = Calculator.calculateVelocity3D(joint1, joint2, time1, time2);// / (time2 - time1) * 1000;
 		double v2 = Calculator.calculateVelocity3D(joint2, joint3, time2, time3);// / (time3 - time2) * 1000;
 		
-		return (v2 - v1) / (time3 - time2) * 1000;
+		return (v2 - v1) / ((time3 - time2));
 	}
 	
 	// Calculates velocity between 2 positions
 	// v = s / t
 	public static double calculateVelocity(double position1, double position2, long time1, long time2) {
-//		System.out.printf("%f %f %d %d %f \n", position1, position2, time1, time2, ((position2 - position1) / (time2 - time1) * 1000));
-		return (position2 - position1) / (time2 - time1) * 1000;
+//		System.out.printf("%f %f %d %d %f %f\n", position1, position2, time1, time2, ((position2 - position1) / (time2 - time1) * 1000), d);
+		return (position2 - position1) / (time2 - time1);
 	}
 	
 	// Calculates acceleration between 2 positions (position2 and position3) having a velocities
@@ -44,7 +44,7 @@ public class Calculator {
 		double v1 = Calculator.calculateVelocity(position1, position2, time1, time2);
 		double v2 = Calculator.calculateVelocity(position2, position3, time2, time3);
 		
-		return (v2 - v1) / (time3 - time2) * 1000;
+		return (v2 - v1) / (time3 - time2);
 	}
 	
 }
